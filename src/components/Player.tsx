7266bfd703b1f0170029a7b8a35a612dbcd2b379
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-06-07 07:33:05 +0300

    Added Logic For Play/Pause Track With Expo-AV

--- src/components/Player.tsx
+++ src/components/Player.tsx
@@ -1,20 +1,76 @@
-import { View, Text, StyleSheet, Image } from "react-native";
+import { View, Text, StyleSheet, Image, TouchableOpacity } from "react-native";
 import { Ionicons } from "@expo/vector-icons";
 import { usePlayerContext } from "../contexts/PlayerContext";
+import { useEffect, useState } from "react";
+import { AVPlaybackStatus, Audio } from "expo-av";
+import { Sound } from "expo-av/build/Audio";
 
 const Player = () => {
+  const [sound, setSound] = useState<Sound | null>(null);
+  const [isPlaying, setIsPlaying] = useState<boolean>(false);
   const { playTrack } = usePlayerContext();
+
+  const handlePlayTrack = async () => {
+    if (sound) {
+      await sound.unloadAsync();
+    }
+
+    if (!playTrack?.preview_url) {
+      return;
+    }
+
+    const { sound: newSound } = await Audio.Sound.createAsync({
+      uri: playTrack.preview_url,
+    });
+    setSound(newSound);
+
+    newSound.setOnPlaybackStatusUpdate(onPlaybackStatusUpdate);
+    await newSound.playAsync();
+  };
+
+  useEffect(() => {
+    if (playTrack) {
+      handlePlayTrack();
+    }
+
+    return () => {
+      if (sound) {
+        sound.unloadAsync();
+      }
+    };
+  }, [playTrack]);
 
   if (!playTrack) {
     return null;
   }
 
-  const image = playTrack.album.images?.[0];
+  const onPlaybackStatusUpdate = (status: AVPlaybackStatus) => {
+    if (!status.isLoaded) {
+      return;
+    }
 
+    setIsPlaying(status.isPlaying);
+  };
+  const handlePauseTrack = async () => {
+    if (!sound) {
+      return;
+    }
+
+    if (isPlaying) {
+      await sound.pauseAsync();
+    } else {
+      await sound.playAsync();
+    }
+  };
   return (
     <View style={styles.container}>
       <View style={styles.player}>
-        {image && <Image source={{ uri: image.url }} style={styles.image} />}
+        {playTrack.album.images?.[0] && (
+          <Image
+            source={{ uri: playTrack.album.images?.[0].url }}
+            style={styles.image}
+          />
+        )}
 
         <View style={{ flex: 1 }}>
           <Text style={styles.title}>{playTrack.name}</Text>
@@ -27,12 +83,17 @@ const Player = () => {
           color={"white"}
           style={{ marginHorizontal: 10 }}
         />
-        <Ionicons
+        <TouchableOpacity
           disabled={!playTrack?.preview_url}
-          name={"play"}
-          size={22}
-          color={playTrack?.preview_url ? "white" : "gray"}
-        />
+          onPress={handlePlayTrack}
+        >
+          <Ionicons
+            onPress={handlePauseTrack}
+            name={isPlaying ? "play" : "pause"}
+            size={22}
+            color={playTrack?.preview_url ? "white" : "gray"}
+          />
+        </TouchableOpacity>
       </View>
     </View>
   );
