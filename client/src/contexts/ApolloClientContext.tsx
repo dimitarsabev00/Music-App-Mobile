6bb27a67fd90d7a8598d3a24399c233679da8166
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-06-11 10:49:20 +0300

    Fix Bug With Authorization In ApolloClient Configuration In ApolloClientContext

--- client/src/contexts/ApolloClientContext.tsx
+++ client/src/contexts/ApolloClientContext.tsx
@@ -5,7 +5,7 @@ const client = new ApolloClient({
   uri: "https://dertka.eu-central-a.ibm.stepzen.net/api/hopping-possum/__graphql",
   headers: {
     Authorization:
-      "dertka::local.net+1000::4debfbca846a56ad09e327667b661df10846c2a2094c6e86a2c91a24b79deff5",
+      "Apikey dertka::local.net+1000::4debfbca846a56ad09e327667b661df10846c2a2094c6e86a2c91a24b79deff5",
   },
   cache: new InMemoryCache(),
 });
