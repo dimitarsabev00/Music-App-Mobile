ce5e80164209cfffa4f0afd0329ac1e8d0dafff5
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-06-11 10:49:38 +0300

    Get Recommendations Data From Spotify API & Render In Home Screen

--- client/src/app/(tabs)/index.tsx
+++ client/src/app/(tabs)/index.tsx
@@ -1,8 +1,49 @@
-import { FlatList } from "react-native";
-import { tracks } from "@/src/data/tracks";
+import { ActivityIndicator, FlatList } from "react-native";
 import TrackListItem from "@/src/components/TrackListItem";
+import { Text } from "@/src/components/Themed";
+import { gql, useQuery } from "@apollo/client";
+
+const getRecommendations = gql`
+  query GetRecommendations($genres: String!) {
+    recommendations(seed_genres: $genres) {
+      tracks {
+        id
+        name
+        preview_url
+        artists {
+          id
+          name
+        }
+        album {
+          id
+          name
+          images {
+            url
+            height
+            width
+          }
+        }
+      }
+    }
+  }
+`;
 
 const Home = () => {
+  const { data, loading, error } = useQuery(getRecommendations, {
+    variables: { genres: "pop" },
+  });
+
+  if (loading) {
+    return <ActivityIndicator style={{marginTop: 20}}/>;
+  }
+
+  if (error) {
+    return <Text>Failed to fetch recommendations. {error.message}</Text>;
+  }
+  
+  const tracks = data?.recommendations?.tracks || [];
+
+
   return (
     <FlatList
       data={tracks}
