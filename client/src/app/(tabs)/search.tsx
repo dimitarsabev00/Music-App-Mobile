68bb0bd57144839d83934457abc1340f7ee7f25e
Dimitar Sabev <dimitarsabev00@gmail.com>
2024-06-11 15:30:07 +0300

    Added Search Functionality WIth Spotify API & Render In Search Screen

--- client/src/app/(tabs)/search.tsx
+++ client/src/app/(tabs)/search.tsx
@@ -1,13 +1,53 @@
-import { View, Text, StyleSheet, TextInput, FlatList } from "react-native";
+import {
+  View,
+  Text,
+  StyleSheet,
+  TextInput,
+  FlatList,
+  ActivityIndicator,
+} from "react-native";
 import React, { useState } from "react";
 import { FontAwesome } from "@expo/vector-icons";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { tracks } from "@/src/data/tracks";
 import TrackListItem from "@/src/components/TrackListItem";
+import { gql, useQuery } from "@apollo/client";
+
+const searchQuery = gql`
+  query SearchQuery($q: String!) {
+    search(q: $q) {
+      tracks {
+        items {
+          id
+          name
+          preview_url
+          artists {
+            id
+            name
+          }
+          album {
+            id
+            name
+            images {
+              url
+              width
+              height
+            }
+          }
+        }
+      }
+    }
+  }
+`;
 
 const Search = () => {
   const [search, setSearch] = useState("");
 
+  const { data, loading, error } = useQuery(searchQuery, {
+    variables: { q: search },
+  });
+
+  const tracks = data?.search?.tracks?.items || [];
   return (
     <SafeAreaView style={styles.container}>
       <View style={styles.header}>
@@ -26,12 +66,13 @@ const Search = () => {
         )}
       </View>
 
+      {loading && <ActivityIndicator />}
+      {error && <Text>Failed to fetch tracks</Text>}
+
       <FlatList
-        data={tracks.filter((track) =>
-          track.name.toLowerCase().includes(search.toLowerCase())
-        )}
+        data={tracks}
         renderItem={({ item }) => <TrackListItem track={item} />}
-        keyExtractor={(item) => item.id.toString()}
+        showsVerticalScrollIndicator={false}
       />
     </SafeAreaView>
   );
